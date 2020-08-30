581779ff974c271957c46a857e1e62791697acda
heyimmatt <mrmcfarland@gmail.com>
2020-08-30 12:20:37 -0700

    Add jumbotron and links to the Nav component

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Jumbotron } from 'reactstrap';
 
 export default function Nav() {
   return (
     <div>
-      Hello
+      <Jumbotron>
+        <h1 className="display-3">Microblog</h1>
+        <p className="lead">Do some blogging!</p>
+        <a href="/" className="mr-3">Blog</a>
+        <a href="/new">Add a new post</a>
+      </Jumbotron>
     </div>
   )
 }
